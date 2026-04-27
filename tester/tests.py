84b3b65c0434d9b4b6e69f950ff468b34293db0c
OkMocassin <leopvvv@gmail.com>
2026-04-27 17:06:46 +0200

    Update tests.py

--- tester/tests.py
+++ tester/tests.py
@@ -1,1 +1,29 @@
+from tester.client import get
 
+URL = "https://api.agify.io?name=michael"
+
+def test_status():
+    r, _ = get(URL)
+    return r.status_code == 200
+
+def test_json():
+    r, _ = get(URL)
+    return r.headers["Content-Type"].startswith("application/json")
+
+def test_fields():
+    r, _ = get(URL)
+    data = r.json()
+    return "name" in data and "age" in data
+
+def test_types():
+    r, _ = get(URL)
+    data = r.json()
+    return isinstance(data["age"], int)
+
+def test_invalid():
+    r, _ = get("https://api.agify.io")
+    return r.status_code in [400, 422]
+
+def test_latency():
+    _, latency = get(URL)
+    return latency < 3000
