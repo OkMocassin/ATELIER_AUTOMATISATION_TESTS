f7ab4e161ea9aeeb6942a206fab27278b146ccda
OkMocassin <leopvvv@gmail.com>
2026-04-27 17:06:28 +0200

    Update client.py

--- tester/client.py
+++ tester/client.py
@@ -1,1 +1,11 @@
-...
+import requests
+import time
+
+def get(url, timeout=3):
+    start = time.time()
+    try:
+        r = requests.get(url, timeout=timeout)
+        latency = (time.time() - start) * 1000
+        return r, latency
+    except Exception:
+        return None, None
